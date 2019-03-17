3d8e8c59f4fac6941014a40bed12035ba17d4301
Kashif Jamal Soofi <kashif.soofi+git@gmail.com>
2019-03-17 04:42:46 +0000

    bootstrap project

---
+++ MessageBoard.Api.AcceptanceTests/TestServerFixture.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+
+namespace MessageBoard.Api.AcceptanceTests
+{
+    public class TestServerFixture : IDisposable
+    {
+        private readonly TestServer _testServer;
+        public HttpClient Client { get; }
+
+        public TestServerFixture()
+        {
+            var builder = new WebHostBuilder()
+                .UseSolutionRelativeContentRoot("MessageBoard.Api")
+                .UseEnvironment("Development")
+                .UseStartup<Startup>();
+
+            _testServer = new TestServer(builder);
+            Client = _testServer.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+            _testServer.Dispose();
+        }
+    }
+}
---
+++ MessageBoard.Api/Controllers/MessagesController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MessageBoard.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new List<string> { "value1", "value2" });
+        }
+    }
+}
