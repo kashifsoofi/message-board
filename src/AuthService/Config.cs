b356abba76e7c6ac648381c8e541dd4f30369d7c
Kashif Jamal Soofi <kashif.soofi@tandem.co.uk>
2021-06-10 12:41:55 +0100

    Add auth service using IdentityServer4, added docker-compose file

---
+++ src/AuthService/Config.cs
@@ -0,0 +1,78 @@
+﻿namespace AuthService
+{
+    using System.Collections.Generic;
+    using System.Security.Claims;
+    using IdentityModel;
+    using IdentityServer4.Models;
+    using IdentityServer4.Test;
+
+    public static class Config
+    {
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("api1", "My API")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                }
+            };
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(),
+                new IdentityResource
+                {
+                    Name = "role",
+                    UserClaims = new List<string> {"role"}
+                }
+            };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new[]
+            {
+                new ApiResource
+                {
+                    Name = "api1",
+                    DisplayName = "API #1",
+                    Description = "Allow the application to access API #1 on your behalf",
+                    Scopes = new List<string> {"api1.read", "api1.write"},
+                    ApiSecrets = new List<Secret> {new Secret("ScopeSecret".Sha256())},
+                    UserClaims = new List<string> {"role"}
+                }
+            };
+
+        public static List<TestUser> TestUsers =>
+            new List<TestUser> {
+                new TestUser {
+                    SubjectId = "5BE86359-073C-434B-AD2D-A3932222DABE",
+                    Username = "admin",
+                    Password = "password",
+                    Claims = new List<Claim> {
+                        new Claim(JwtClaimTypes.Email, "admin@messageboard.io"),
+                        new Claim(JwtClaimTypes.Role, "admin")
+                    }
+                }
+            };
+    }
+}
